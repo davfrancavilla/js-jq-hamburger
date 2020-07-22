7a06fa27ff9558e7b6d958b8749c052e3e618352
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-22 15:05:33 +0200

    Menù funzionante

---
+++ main.js
@@ -0,0 +1,24 @@
+$('.fa-bars').click(function(){
+    $('.hamburger-menu').show();
+});
+
+$('.fa-times').click(function(){
+    $('.hamburger-menu').hide();
+});
+
+
+
+
+// VERSIONE CON VARIABILE BOOLEANA
+
+// var check = false;
+//
+// $('header a i').click(function(){
+//     if (check){
+//         $('.hamburger-menu').hide();
+//         check = false;
+//     } else {
+//         $('.hamburger-menu').show();
+//         check = true;
+//     }
+// })
